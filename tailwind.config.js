6dd6c1557fef947216437bbb50da0f83a57399b5
berlysia <berlysia@gmail.com>
2021-05-05 18:11:56 +0900

    add blink animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-blink": {
+          "0%": {
+            opacity: 0,
+          },
+          "50%": {
+            opacity: 1,
+          },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        "fade-blink": "fade-blink 3s ease-in-out infinite",
+      },
+    },
   },
   variants: {
     extend: {
