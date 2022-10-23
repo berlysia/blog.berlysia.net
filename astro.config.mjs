b08ec656f99f3e99da47d0cf47f75fc38472a181
berlysia <berlysia@gmail.com>
2022-10-23 18:51:24 +0900

    setup vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+// https://github.com/withastro/astro/issues/5172 動かない
+// import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
