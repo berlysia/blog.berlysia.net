7a7933ec1daa5dc729ed3f3171c0daee6768c26c
berlysia <berlysia@gmail.com>
2022-10-26 03:51:04 +0900

    remove unused config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  exportPathMap: (
-    _defaultPathMap,
-    { _dev, _dir, _outDir, _distDir, _buildId }
-  ) => {
-    return {
-      "/": { page: "/" },
-    };
-  },
   images: {
     domains: ["b.hatena.ne.jp"],
   },
