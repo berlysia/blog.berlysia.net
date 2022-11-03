3f26d947d52b030dc1fd600fc2b360a4cf4be876
berlysia <berlysia@gmail.com>
2022-11-03 14:16:39 +0900

    set viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
