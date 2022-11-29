ca0c8c7498221a266ddd84a04bf77a7a919b92c6
berlysia <berlysia@gmail.com>
2022-11-30 02:05:25 +0900

    feat: 記事中からアセットを相対参照する

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,16 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import mdx from "@astrojs/mdx";
-
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "@jsdevtools/rehype-toc";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/static";
+import image from "@astrojs/image";
+import { resolve, relative, dirname, basename, isAbsolute } from "node:path";
 
-function renameFootnoteSectionPlugin() {
+function rehypeRenameFootnoteSectionPlugin() {
   const isEl = (x) => x.type === "element";
   return function (tree) {
     const footnoteHeadingEl = tree.children
@@ -26,19 +26,62 @@ function renameFootnoteSectionPlugin() {
   };
 }
 
+function remarkResolveAssets() {
+  return function remarkResolveAssetsImpl(tree, file) {
+    // src/pages/blog/entry/foo.mdx => /blog/entry/foo
+    const tmp = relative(resolve(file.cwd, "src/pages"), file.history[0]);
+    const baseDir = resolve("/", dirname(tmp), basename(tmp, ".mdx"));
+
+    function walk(curr) {
+      if ("children" in curr) {
+        for (const x of curr.children) {
+          switch (x.type) {
+            case "image": {
+              // `![](./image.png)` => `![](/blog/entry/foo/image.png)` in src/pages/blog/entry/foo.mdx
+              if (isAbsolute(x.url)) continue;
+              const resolved = resolve(baseDir, x.url);
+              x.url = resolved;
+              break;
+            }
+            case "mdxJsxFlowElement": {
+              // `<Some src="./image.png" />` => `<Some src="/blog/entry/foo/image.png" />` in src/pages/blog/entry/foo.mdx
+              const srcAttr = x.attributes?.find(
+                (at) => at.type === "mdxJsxAttribute" && at.name === "src"
+              );
+              if (!srcAttr || isAbsolute(srcAttr.value)) continue;
+              const resolved = resolve(baseDir, srcAttr.value);
+              srcAttr.value = resolved;
+              break;
+            }
+          }
+          walk(x);
+        }
+      }
+    }
+    walk(tree);
+  };
+}
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     mdx({
-      // 動かない。 https://github.com/withastro/astro/pull/5432 リリースされたら renameFootnoteSectionPlugin を消す
-      remarkRehype: { footnoteLabel: "脚注" },
-      remarkPlugins: [],
+      // 動かない。 https://github.com/withastro/astro/pull/5432 リリースされたら rehypeRenameFootnoteSectionPlugin を消す
+      remarkRehype: {
+        footnoteLabel: "脚注",
+      },
+      remarkPlugins: [remarkResolveAssets],
       rehypePlugins: [
         rehypeSlug,
-        [rehypeAutolinkHeadings, { behavior: "wrap" }],
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: "wrap",
+          },
+        ],
         [rehypeToc, {}],
         rehypeAccessibleEmojis,
-        renameFootnoteSectionPlugin,
+        rehypeRenameFootnoteSectionPlugin,
       ],
       extendDefaultPlugins: true,
     }),
@@ -49,8 +92,12 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    image({
+      // https://github.com/GoogleChromeLabs/squoosh/issues/1242
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
   ],
   markdown: {},
-  output: "server",
+  output: "static",
   adapter: vercel(),
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference -- required by framework
+/// <reference types="@astrojs/image/client" />
+
 interface ImportMetaEnv {
   readonly VITE_GOOGLE_ANALYTICS_ID: string;
 }
