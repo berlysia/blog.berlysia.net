6bd9c19cc72981261156cf338307f56438b3eef9
berlysia <berlysia@gmail.com>
2022-12-02 03:24:18 +0900

    update snapshots

--- test/vrt/test.ts
+++ test/vrt/test.ts
@@ -1,11 +1,26 @@
-import { existsSync } from "node:fs";
 import { test, expect } from "@playwright/test";
 
 // const paths = ["/", "/blog", "/blog/entry/test"];
-const paths = ["/blog", "/blog/entry/test"];
+const paths = ["/blog", "/blog/entry/test", "/blog/entry/vertical"];
 
 for (const pathname of paths) {
   test(`VRT: ${pathname}`, async ({ page }) => {
+    await page.goto(`http://localhost:3000${pathname}`);
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      scale: "device",
+    });
+  });
+}
+
+for (const pathname of paths) {
+  test(`VRT-SP: ${pathname}`, async ({ page }) => {
+    await page.setViewportSize({
+      // iPhone 12 Pro
+      width: 390,
+      height: 844,
+    });
     await page.goto(`http://localhost:3000${pathname}`);
     await expect(page).toHaveScreenshot({
       animations: "disabled",
