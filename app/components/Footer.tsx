dc301d9ad3a768ea214915384c46e74190fefb7b
berlysia <berlysia@gmail.com>
2024-02-14 03:46:34 +0900

    fix footer writing mode

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,8 @@
-import { css } from "hono/css";
 import type { PropsWithChildren } from "hono/jsx";
 
-type Props = PropsWithChildren;
+type Props = PropsWithChildren<{ className?: string }>;
 
-export default function Footer({ children }: Props) {
+export default function Footer({ children, className }: Props) {
   return (
     <footer className="footer tw-is-full tw-bs-14 tw-flex tw-justify-center">
       <div className="tw-flex tw-flex-col tw-max-w-screen-lg tw-w-full tw-h-full tw-relative tw-px-2">
