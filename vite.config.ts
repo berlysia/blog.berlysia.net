78233f03cd06f0037b63c6facfa99b3e8cf28356
berlysia <berlysia@gmail.com>
2024-02-17 18:38:17 +0900

    update hono/vite-ssg

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,43 @@ import honox from "honox/vite";
 import client from "honox/vite/client";
 import { defineConfig } from "vite";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import ssgBuild from "@hono/vite-ssg";
+
+const entry = "app/server.ts";
 
 export default defineConfig(({ mode, command }) => {
-  const plugins = [mode === "client" ? client() : honox(), viteCommonjs()];
-
-  return {
-    build: {
-      rollupOptions: {
-        input: ["./app/style.css"],
-        output: {
-          assetFileNames: "static/assets/[name]-[hash].[ext]",
-        },
-      },
-    },
+  const common = {
     ssr: {
       external: ["debug", "acorn-jsx"],
     },
     optimizeDeps: {
       entries: ["./app/server.ts"],
     },
-    plugins,
     resolve: {
       alias: [{ find: /#/, replacement: "/app/" }],
     },
+  };
+
+  if (mode === "client") {
+    return {
+      ...common,
+      build: {
+        rollupOptions: {
+          input: ["./app/style.css"],
+          output: {
+            assetFileNames: "static/assets/[name]-[hash].[ext]",
+          },
+        },
+      },
+      plugins: [client(), viteCommonjs()],
+    };
+  }
+
+  return {
+    ...common,
+    build: {
+      emptyOutDir: false,
+    },
+    plugins: [honox(), ssgBuild({ entry }), viteCommonjs()],
   };
 });
