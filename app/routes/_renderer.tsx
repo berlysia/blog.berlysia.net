1e481f082b85ca25a57e126565356d4dde5bec3b
berlysia <berlysia@gmail.com>
2024-02-23 20:34:18 +0900

    activate stream

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -3,38 +3,46 @@ import { Style } from "hono/css";
 import { jsxRenderer } from "hono/jsx-renderer";
 import { Script } from "honox/server";
 
-export default jsxRenderer(({ children, title, description }) => {
-  return (
-    <html lang="ja">
-      <head>
-        <meta charset="utf8" />
-        <title>
-          {title ? `${title} | blog.berlysia.net` : "blog.berlysia.net"}
-        </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {title && <meta property="og:title" content={title} />}
-        <meta property="og:description" content={description ?? ""} />
-        <meta property="og:site_name" content="blog.berlysia.net" />
-        <meta property="og:locale" content="ja_JP" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@berlysia" />
-        <meta
-          name="twitter:title"
-          content={title ? `${title} | blog.berlysia.net` : "blog.berlysia.net"}
-        />
-        <meta name="twitter:description" content={description ?? ""} />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Zen+Kaku+Gothic+New&display=swap"
-          rel="stylesheet"
-        />
+export default jsxRenderer(
+  ({ children, title, description }) => {
+    return (
+      <html lang="ja">
+        <head>
+          <meta charset="utf8" />
+          <title>
+            {title ? `${title} | blog.berlysia.net` : "blog.berlysia.net"}
+          </title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          {title && <meta property="og:title" content={title} />}
+          <meta property="og:description" content={description ?? ""} />
+          <meta property="og:site_name" content="blog.berlysia.net" />
+          <meta property="og:locale" content="ja_JP" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:site" content="@berlysia" />
+          <meta
+            name="twitter:title"
+            content={
+              title ? `${title} | blog.berlysia.net` : "blog.berlysia.net"
+            }
+          />
+          <meta name="twitter:description" content={description ?? ""} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Zen+Kaku+Gothic+New&display=swap"
+            rel="stylesheet"
+          />
 
-        <ResolveManifest src="app/style.css" type="style" />
-        <Script src="/app/client.ts" />
-        <Style />
-      </head>
-      <body>{children}</body>
-    </html>
-  );
-});
+          <ResolveManifest src="app/style.css" type="style" />
+          <Script src="/app/client.ts" />
+          <Style />
+        </head>
+        <body>{children}</body>
+      </html>
+    );
+  },
+  { stream: true }
+);
